7adb45d3042dce0b799cb54f707572461351012e
Bhavya Bharti <bhavyab@outlook.com>
2017-02-02 13:03:39 -0800

    testing

---
+++ test/gallery-service-test.js
@@ -0,0 +1,68 @@
+'use strict';
+
+describe('Gallery Service', function() {
+
+  beforeEach( () => {
+    angular.mock.module('cfgram');
+    angular.mock.inject(( $rootScope, authService, galleryService, $window, $httpBackend) => {
+      this.$window = $window;
+      this.$rootScope = $rootScope;
+      this.authService = authService;
+      this.galleryService = galleryService;
+      this.$httpBackend = $httpBackend;
+      this.$window.localStorage.setItem('token', 'test token');
+    });
+  });
+
+  describe('galleryService.createGallery()', () => {
+    it('should create a new gallery', () => {
+      let galleryData = {
+        name: 'example gallery',
+        desc: 'example description'
+      };
+
+      let headers = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: 'Bearer test token'
+      };
+
+      this.$httpBackend.expectPOST('http://localhost:8000/api/gallery', galleryData, headers)
+      .respond(200, {
+        _id: '1234',
+        username: 'testuser',
+        name: galleryData.name,
+        desc: galleryData.desc,
+        pics: []
+      });
+
+      this.galleryService.createGallery(galleryData);
+      this.$httpBackend.flush();
+      this.$rootScope.$apply();
+    });
+  });
+  describe('galleryService.fetchGalleries()', () =>{
+    let headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer test token'
+    };
+    this.$httpBackend.expectGET('http://localhost:8000/api/gallery', headers)
+    .respond(200);
+    this.galleryService.galleries(galleries);
+  })
+  describe('galleryService.deleteGallery()', () => {
+    it('should delete a gallery', () => {
+      let galleryID = 'testid';
+      let headers = {
+        Authorization: 'Bearer test token',
+        Accept: "application/json, text/plain, */*",
+      };
+
+      this.$httpBackend.expectDELETE('http://localhost:8000/api/gallery/testid', headers).respond(204);
+      this.galleryService.deleteGallery(galleryID);
+      this.$httpBackend.flush();
+      this.$rootScope.$apply();
+    })
+  });
+});
